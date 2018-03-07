8c41e359d1e8d6d3109efd18cc696b0166dca224
hao3304 <hao3304@qq.com>
2018-03-07 17:35:39 +0800

    xxxxxx

--- src/tools/texTool.js
+++ src/tools/texTool.js
@@ -25,9 +25,6 @@ exports.getTex = function(max,min,col,row){
             \\usepackage{mathrsfs}
             \\usepackage{amsmath}
             \\usepackage{bm}
-            \\begin{CJK}{UTF8}{gkai}
-            \\LARGE 口算练习题 \\\\
-            \\end{CJK}
             \\LARGE
             \\begin{align*}
             ${data.join("")}
